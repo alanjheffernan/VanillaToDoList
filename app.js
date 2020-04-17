a961a4f623208860603a7eb566a909280afbd5d0
AHE54 <alan.heffernan@sky.uk>
2020-04-17 16:34:30 +0100

    added toggle for completed todos started local storage

--- app.js
+++ app.js
@@ -45,18 +45,51 @@ function deleteCheck(event) {
         const todo = item.parentElement;
         //Animation
         todo.classList.add("fall");
-        todo.addEventListener('transitionend', function(){
+        todo.addEventListener('transitionend', function () {
             todo.remove();
         });
     }
 
     //Check todo
-    if(item.classList[0] === "complete-btn") {
+    if (item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
 
-function filterTodo {
-    const todos = todoList.childNodes;
 }
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (todo.classList.contains("completed")){
+                    todo.style.display = "none";
+                } else {
+                    todo.style.display = "flex";
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTodos(todo){
+    //Check to see if todo is already in local storage
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo)
 }
